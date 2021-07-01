5a5a3abbf6a8cbb9e03918cc8b29e46bdf72e8ce
Senott <senott@yahoo.com>
2021-07-01 16:45:34 -0300

    Create products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,33 @@
 class ProductsController < ApplicationController
+  before_action :set_restaurants, only: %i[new]
+
   def index
     @products = Product.includes(:restaurant).all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to :index, notice: 'Product created successfully.' }
+      else
+        format.html { render :new, alert: 'Product creation failed.'}
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :value, :restaurant_id)
+  end
+
+  def set_restaurants
+    @restaurants = Restaurant.all.order(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products, only: %i[index]
+  resources :products, only: %i[index new create]
   resources :restaurants
   devise_for :users
   root 'home#index'
